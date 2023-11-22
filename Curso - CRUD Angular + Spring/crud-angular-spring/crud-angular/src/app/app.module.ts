285331315f8cd469cef506f5299b333a34bb57de
Lucas Camargo da Silva <contatolcamargo@gmail.com>
2023-11-22 18:41:08 -0300

    22/11

--- Curso - CRUD Angular + Spring/crud-angular-spring/crud-angular/src/app/app.module.ts
+++ Curso - CRUD Angular + Spring/crud-angular-spring/crud-angular/src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatToolbarModule } from '@angular/material/toolbar';
-
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,8 +12,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
