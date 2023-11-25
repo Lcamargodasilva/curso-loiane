8fc5196618572ac4d80b3ce502b4f9d25f983b8f
Lucas Camargo da Silva <contatolcamargo@gmail.com>
2023-11-24 21:25:05 -0300

    24/11

--- Curso - CRUD Angular + Spring/crud-angular-spring/crud-spring/src/main/java/controller/CourseController.java
+++ Curso - CRUD Angular + Spring/crud-angular-spring/crud-spring/src/main/java/controller/CourseController.java
@@ -2,23 +2,23 @@ package controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import CourseRepository.CourseRepository;
+import lombok.AllArgsConstructor;
 import model.Course;
-import repository.CourseRepository;
 
 @RestController
 @RequestMapping("/api/courses")
+@AllArgsConstructor
 public class CourseController {
 
     private final CourseRepository courseRepository;
 
-    @Autowired
-    public CourseController(CourseRepository courseRepository) {
-        this.courseRepository = courseRepository;
+    public CourseController() {
+        this.courseRepository = null;
     }
 
     @GetMapping
--- Curso - CRUD Angular + Spring/crud-angular-spring/crud-spring/src/main/java/model/Course.java
+++ Curso - CRUD Angular + Spring/crud-angular-spring/crud-spring/src/main/java/model/Course.java
@@ -10,14 +10,14 @@ import lombok.Data;
 @Data
 @Entity
 public class Course {
-
+    
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
+    
     @Column(length = 200, nullable = false)
     private String name;
-
+    
     @Column(length = 20, nullable = false)
     private String category;
 
